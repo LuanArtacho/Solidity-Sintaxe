8b03d27af33d6d60f46321e7ad90b68cbda175aa
Luan Artacho <luan.artacho@gmail.com>
2022-09-22 02:50:14 -0300

    Update file 8 - Listas/Arrays

--- 8 - Arrays(listas).sol
+++ 8 - Arrays(listas).sol
@@ -1,13 +1,16 @@
 pragma solidity ^0.8.0;
 
+//Abaixo segue um Ex de CRUD
+
 //Create
 //Read
 //Update
 //Delete
 
-//Arrays de Arrays
-
+//Arrays são listas de um único tipo de dado e as utilidades e aplicações são praticamente infinitas.
 //Arrays no Solidity pode ter somente um tipo de dado na lista, uma lista de nums, uma lista de strings..
+
+//Arrays de Arrays
 
 contract listas {
     string[] nomes; // assim é uma lista infinita..
@@ -31,4 +34,22 @@ contract listas {
     function deletaNome(uint _position) external {
         delete nomes[_position]; // ve a posicao e deleta
     }
+
+    
+    //Arrays Multidimencionais
+    //Parecido com tabelas ou outras estruturas multidi-mencionais como tensores, também é possível construir "listas de listas" em solidity.
+
+    //Lista de listas de shitcoins
+
+    string[][] listas_de_shitcoins;
+
+    listas_de_shitcoins.push(["Safemoon", "Elomoon"])
+    listas_de_shitcoins.push(["SafeUniverse", "Elondoge", "REAU"])
+
+    listas_de_shitcoins[0][0] == "Safemoon";
+    listas_de_shitcoins[1][2] == "REAU";
+
+    //Nesse caso temos uma lista que contém outras listas de shitcoins.
+    //Para acessar um valor guardado em uma lista de listas precisamos encontrá-lo pelos indices da seguinte forma: 
+    //nomedalista[primeiro_indice][segundo_indice].
 }
