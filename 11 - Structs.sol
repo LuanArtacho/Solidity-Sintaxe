8295ce415a5b7ac4f41c9a4c31b065295c91c6a0
Luan Artacho <luan.artacho@gmail.com>
2022-09-28 01:38:51 -0300

    Update Structs and Readme

--- 11 - Structs.sol
+++ 11 - Structs.sol
@@ -1,30 +1,46 @@
 pragma solidity ^0.8.0;
 
+/**
+Já pensou ter que gerenciar um sistema inteiro usando Dicionários e mais Dicionários? Seria confuso e provavelmente não escalável.
+
+Structs são muito parecidos com Objetos de outras linguagens, mas somente parecidos mesmo porque não tem métodos ou tanta liberdade.
+
+com Structs podemos definir uma estrutura de variáveis. Imagine que você precisa cadastrar um usuário e cada usuário terá nome, 
+endereço da carteira, id, saldo e outros dados... gerenciar muitos usuários e cada um tendo tantos dados... sem Structs eu não sei como você faria!
+*/
+
+//Struct são como objetos, é uma estrutura de variaveis..
+
 contract marketplace {
     
-    struct Usuario {
+    struct Usuario { //definindo a estrutura, e criando os tipos de dados da estrututa.
         address wallet;
         uint id;
         string name;
     }
+    // address, é um tipo de dado, uint é um tipo de dado, struct é uma colecao de dados mas tambem é um tipo de dado,
+    // agora precisamos instanciar esse dado.
 
-    Usuario[] usuarios;
-    function addUsuarioImplicit(uint _id, string memory _name) external {
-        Usuario memory usuario1 = Usuario(msg.sender, _id, _name);
-        usuarios.push(usuario1);
+    Usuario[] usuarios; // lista de Struct
+    // Forma 1 - GAS FOI 92057 WEI
+    function addUsuarioImplicit(uint _id, string memory _name) external { //passamos os dados implicitamente.. sem definir.. gasta um pouco mais de GAS
+        Usuario memory usuario1 = Usuario(msg.sender, _id, _name); // instanciando a struct, a wallet é pega do msg.sender | memory é usado pois estamos dentro de uma funcao e vamos usalo 'temporariamente'.. nn vamos guardalo na blockchain
+        usuarios.push(usuario1); // adicionando na lista de struct
     }
 
-    function addUsuarioExplicit(uint _id, string memory _name) external{
-        Usuario memory usuario2 = Usuario({wallet: msg.sender, id: _id, name: _name});
+    // Forma 2 - GAS FOI 94852 WEI
+    function addUsuarioExplicit(uint _id, string memory _name) external{ // inserçao de forma explicita
+        Usuario memory usuario2 = Usuario({wallet: msg.sender, id: _id, name: _name}); ///passando de forma explicita, chave->valor
         usuarios.push(usuario2);
     }
     
+    // Forma 3 - Modo mais economico(SEMPRE SEJA ECONOMICO!) - GAS FOI 94908 WEI
     function addUsuarioBarato(uint _id, string memory _name) external {
-        usuarios.push(Usuario(msg.sender, _id, _name));
+        usuarios.push(Usuario(msg.sender, _id, _name)); // passando direto pra lista
     }
 
-    function nome(uint _position) external view returns(string memory){
-        return usuarios[_position].name;
+    function nome(uint _position) external view returns(string memory){ // funcao apenas pra ler o nome..
+        return usuarios[_position].name; //pega o indice da lista e retorna o nome...
     }
 }
 
@@ -47,7 +63,8 @@ struct Usuario {
 /**
 Instanciando e inserindo dados.
 Para usar uma estrutura precisamos instancia-la em uma outra variável e passamos os valores para as variáveis contidas na estrutura na hora em que criamos a instância. 
-No exemplo abaixo temos uma função chamada foo e dentro dela instanciamos o struct Usuario dentro da variável usuario1 e usuario2 usando posições implícitas, que nesse caso são o primeiro parâmetro é o endereço, segundo parâmetro é a pontuação e o terceiro parâmetro o nome_._
+No exemplo abaixo temos uma função chamada foo e dentro dela instanciamos o struct Usuario dentro da variável usuario1 e usuario2 usando posições implícitas, 
+que nesse caso são o primeiro parâmetro é o endereço, segundo parâmetro é a pontuação e o terceiro parâmetro o nome_._
 O usuario3 foi instanciado usando dados explícitos, passamos os dados no formato dicionário com chave e valor para indicar em qual variável (chave) será guardado cada dado (valor).
 
 Ler, Atualizar e Deletar dados
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Solidity-Sintaxe
 
-#PARA EXECUTAR OS CODIGOS USE O 'https://remix.ethereum.org/' E NÃO ESQUEÇA DE FAZER O DEPLOY DO CONTRATO
+- PARA EXECUTAR OS CODIGOS USE O 'https://remix.ethereum.org/' E NÃO ESQUEÇA DE FAZER O DEPLOY DO CONTRATO
 
-Pare para ler os comentarios que deixei, podem te ajudar a esclarecer a lógica e o pensamento por trás da função.
+- Pare para ler os comentarios que deixei, podem te ajudar a esclarecer a lógica e o pensamento por trás da função.
+
+ 
